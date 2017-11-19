623c7b7f62feff63e6467f96d7271da9550fba05
ecoulson <ivansonofcoul@gmail.com>
2017-11-18 20:32:08 -0800

    continued to create MYSQL tests

--- tests/mysql.test.js
+++ tests/mysql.test.js
@@ -3,6 +3,29 @@ const createDatabaseConnection = require('../src/util/mysql');
 const expect = chai.expect;
 
 describe('MYSQL Test Suite', async function() {
-	const MYSQL = await createDatabaseConnection('foo');
-	
+	const MYSQL = await createDatabaseConnection('test');
+
+	// testing code
+// const Animal = sequelize.define('animals', {
+// 	species: {
+// 		type: Sequelize.STRING,
+// 	},
+// 	sound: {
+// 		type: Sequelize.STRING,
+// 	},
+// });
+
+// Animal.sync({force: true}).then(() => {
+// 	logger.info('Table: "animals" succesfully created');
+// 	return Animal.create({
+// 		species: 'Lion',
+// 		sound: 'RAWR',
+// 	});
+// }).then(function () {
+// 	Animal.findAll().then(function handleAnimals(animals) {
+// 		logger.debug(animals);
+// 	}).catch(function handleError(err) {
+// 		logger.error(err);
+// 	});
+// });
 });
