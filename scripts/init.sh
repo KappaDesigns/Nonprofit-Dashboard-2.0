75b7a1d9891493d0d0fd2b8bf4acdb690e19b714
ecoulson <ivansonofcoul@gmail.com>
2017-11-22 17:28:42 -0800

    added script to clone git repo based off of a config paramater

---
+++ scripts/init.sh
@@ -0,0 +1,2 @@
+./scripts/heroku.sh
+node ./scripts/cloneSite
--- src/lib/site.js
+++ src/lib/site.js
@@ -1,10 +1,35 @@
-import { setTimeout } from "timers";
+/**
+ * spec
+ * 1. init
+ * 	pull repo
+ * 	create asset map
+ * 		keys: file name
+ * 		values: read file function
+ * 	start 24 hour dev updater
+ * 	
+ * 2. force update
+ * 	forcefully updates website from the github repo of the true website
+ * 
+ * 3. updatePage
+ * 	takes in the html of an admin panel page
+ * 	parse to ignore the data-admin-only attributes
+ * 	write to file
+ * 	commit changes
+ * 
+ * 4. publish
+ * 	pushes current changes to github
+ * 
+ * 5. revert
+ * 	can revert back to a given commit id
+ *  reverts back a commit if no commit id given
+ * 	can revert back to previous
+ * 
+ * 6. createTimer
+ * 	pulls repo every 24 hours checking for dev changes by a web team
+ * 
+ */
 
-//spec
-// 1. init
-// 2. 
-
-const daytime = 1000 * 60 * 60 * 24;
+const daytime = 1000 * 60 * 60 * 24; //miliseconds in 1 day
 
 async function init() {
 	await pullRepo();
