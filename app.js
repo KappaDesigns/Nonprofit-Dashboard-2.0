5b60cd98e23d8916f500686d75d912b3c44cd970
ecoulson <ivansonofcoul@gmail.com>
2017-11-26 23:51:38 -0800

    added basic idea of what routing should look like

--- app.js
+++ app.js
@@ -2,15 +2,11 @@
 const Express = require('express');
 const app = Express();
 
-//Handling pages task
-// 0. init by passing git clone url
-// 1. download website
-// 2. check every 24 hours and pull down the website
-// 3. edits to the site written to the file in the local git repo
-
-app.get('/', function handleRequest() {
-	
-});
+app.use(require('./src/routes'));
 
 // sets app to listen on a passed port.
-app.listen(process.argv[2]);
+app.listen(process.argv[2] || 8080, function () {
+	console.log('Listenting on port 8080');
+});
+
+//https://github.com/gothinkster/node-express-realworld-example-app/blob/master/app.js
