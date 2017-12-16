061617777302b8927e549c20d151156ea1c7be01
ecoulson <ivansonofcoul@gmail.com>
2017-12-15 17:06:37 -0800

    completed site routing

--- src/routes/api/site-route.js
+++ src/routes/api/site-route.js
@@ -6,9 +6,16 @@ const fs = require('fs');
 const util = require('../../util');
 const Site = require('../../lib/site');
 
-router.get('/:path', function handleReq(req, res, next) {
+/**
+ * @description handles the GETing of all resources within the site
+ */
+router.get('/', function handleReq(req, res, next) {
 	logger.info('Received GET Requeset @ path');
-	const path = util.globalizePath(req.params.path);
+	if (!req.query.hasOwnProperty('path')) {
+		res.status(400).send('No path given for requested resource');
+		return next();
+	}
+	const path = util.globalizePath(req.query.path);
 	fs.stat(path, async function handleAccess(err, stats) {
 		if (err) {
 			logger.info('Error finding file...');
@@ -43,6 +50,9 @@ router.get('/:path', function handleReq(req, res, next) {
 	});
 });
 
+/**
+ * @description handles the updating of data to a given resource
+ */
 router.put('/:path', function handleReq(req, res, next) {
 	logger.info('Received PUT Request @ path');
 	const path = util.globalizePath(req.params.path);
@@ -83,6 +93,9 @@ router.put('/:path', function handleReq(req, res, next) {
 	});
 });
 
+/**
+ * @description updates the site with the current development environment
+ */
 router.post('/sync', async function handleReq(req, res, next) {
 	logger.info('Received POST Request @ sync');
 	try {
@@ -98,6 +111,9 @@ router.post('/sync', async function handleReq(req, res, next) {
 	}
 });
 
+/**
+ * @description updates the github repository with front end user changes
+ */
 router.post('/publish', async function handleReq(req, res, next) {
 	logger.info('Received POST request @ publish');
 	try {
@@ -110,6 +126,23 @@ router.post('/publish', async function handleReq(req, res, next) {
 	}
 });
 
-
+/**
+ * @description handles the reversion of changes to a certain point 
+ */
+router.post('/revert/:hash', async function handleReq(req, res, next) {
+	logger.info('Received POST request @ publish');
+	if (!req.params.hash) {
+		res.status(404).send('No revert hash given');
+		return next();
+	}
+	try {
+		const sha = await Site.revert(req.params.hash);
+		res.status(200).send(sha);
+		return next();
+	} catch (err) {
+		res.status(500).send('Something went horribly wrong');
+		return next();
+	}
+});
 
 module.exports = router;
