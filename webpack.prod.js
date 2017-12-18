2e31933f031628515b7956c69fc5654cb482e7f2
ecoulson <ivansonofcoul@gmail.com>
2017-12-18 13:59:38 -0800

    added manifest plugin to webpack production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const Common = require('./webpack.common');
 const merge = require('webpack-merge');
 const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = merge(Common, {
 	devtool: 'source-map',
@@ -12,5 +13,6 @@ module.exports = merge(Common, {
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production'),
 		}),
+		new ManifestPlugin(),
 	],
 });
