7ea5fe5de5b137d9902b31d6438384a1535e2a3a
ecoulson <evan.m.coulson@gmail.com>
2018-06-23 01:45:55 -0700

    added repository cloner test

--- src/lib/Site/Site.ts
+++ src/lib/Site/Site.ts
@@ -4,17 +4,14 @@ import { RepositoryCloner } from "./Git/RepositoryCloner";
 export class Site {
 	private repository: Repository;
 	private repositoryURL: string;
-	private hasInited: boolean;
 
 	constructor(repositoryURL: string) {
 		this.repositoryURL = repositoryURL;
-		this.hasInited = false;
 	}
 
 	public async init(): Promise<void> {
 		this.repository = await RepositoryCloner.cloneRepository(this.repositoryURL);
 		await this.buildSite();
-		this.hasInited = true;
 	}
 
 	private async buildSite() {
